46b39d2cf341c808333a3ecff896b20dbbfdae60
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-01-21 20:05:38 +0100

    small updates in user model v2

---
+++ TrustMessageApp/Server/Migrations/20250121173757_UpdateUserModelFor2Factor.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateUserModelFor2Factor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PublicKey",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "FailedLoginAttempts",
+                table: "Users",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LockoutEnd",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FailedLoginAttempts",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEnd",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PublicKey",
+                table: "Users",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
