d25ad97c9607356af1077a4f0c75fc517e2c6016
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-01-21 17:48:53 +0100

    x

---
+++ TrustMessageApp/BaseLibrary/Entities/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseLibrary.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Content { get; set; } = string.Empty;
+        public string Signature { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public User? User { get; set; }
+    }
+}
