59c251a375a09b1608c9e6006fc50238369aa45b
ArtemkaGoldMan <artemka.omelchenko@gmail.com>
2025-01-24 17:47:56 +0100

    model valiadtion

--- TrustMessageApp/BaseLibrary/DTOs/ChangePasswordDTO.cs
+++ TrustMessageApp/BaseLibrary/DTOs/ChangePasswordDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace BaseLibrary.DTOs
 {
     public class ChangePasswordDTO
     {
+        [Required]
         public string Username { get; set; } = string.Empty;
+        [Required]
         public string NewPassword { get; set; } = string.Empty;
+        [Required]
         public string TwoFactorCode { get; set; } = string.Empty;
     }
 }
--- TrustMessageApp/BaseLibrary/DTOs/LoginRequestDTO.cs
+++ TrustMessageApp/BaseLibrary/DTOs/LoginRequestDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,11 @@ namespace BaseLibrary.DTOs
 {
     public class LoginRequestDTO
     {
+        [Required]
         public string Username { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
+        [Required]
         public string TwoFactorCode { get; set; } = string.Empty;
     }
 }
